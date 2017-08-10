10d7e64b33089597c1f3977e6611304ec783aaf0
Normand Cyr <normand.cyr@gmail.com>
2017-08-10 17:30:54 -0400

    add matching pattern to pdb code in list

--- fetch-pdb.py
+++ fetch-pdb.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-import urllib.request
+from urllib.request import urlopen
+from re import match, compile
 
 def downloading(filename):
+
     with open(filename) as pdb_list:
+
         for structure in pdb_list:
-            pdb_url = base_url + structure[:4]
-            out_file_name = structure[:4] + '.pdb'
-            with urllib.request.urlopen(pdb_url) as response, open(out_file_name, 'wb') as out_file:
-                data = response.read()
-                out_file.write(data)
-                print('Downloading {} as {}.'.format(structure[:4], out_file_name))
+
+            if match(pdbPattern, structure):
+
+                pdb_url = base_url + structure[:4]
+                out_file_name = structure[:4] + '.pdb'
+
+                with urlopen(pdb_url) as response, open(out_file_name, 'wb') as out_file:
+
+                    data = response.read()
+                    out_file.write(data)
+                    print('Downloading {} as {}.'.format(structure[:4], out_file_name))
+
     return(data)
 
 if __name__ == '__main__':
+
     filename = 'pdblist'
+    pdbPattern = compile('\d.{3}')
     base_url = 'http://www.pdb.org/pdb/download/downloadFile.do?fileFormat=pdb&compression=NO&structureId='
     data = downloading(filename)
